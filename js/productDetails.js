01cd5a65c448458e24602aeb301f456ac6d44090
Zydrunask <kniukstaz@gmail.com>
2024-08-13 08:33:01 +0300

    tragedija

---
+++ js/productDetails.js
@@ -0,0 +1,29 @@
+const firstShoppingList = [
+    {
+        id: 1,
+        name: 'Pomidoras',
+        amount: 1000000,
+        unitPrice: 199,
+    },
+    {
+        id: 3,
+        name: 'Agurkas',
+        amount: 2,
+        unitPrice: 50,
+    },
+    {
+        id: 7,
+        name: 'Svogūnas',
+        amount: 1,
+        unitPrice: 45,
+    },
+];
+
+function productDetails(params) {
+    
+}
+const emptyList = [];
+
+
+
+
---
+++ js/shoppingList.js
@@ -0,0 +1,50 @@
+const firstShoppingList = [
+    {
+        id: 1,
+        name: 'Pomidoras',
+        amount: 1000000,
+        unitPrice: 199,
+    },
+    {
+        id: 3,
+        name: 'Agurkas',
+        amount: 2,
+        unitPrice: 50,
+    },
+    {
+        id: 7,
+        name: 'Svogūnas',
+        amount: 1,
+        unitPrice: 45,
+    },
+];
+const singleProductShoppingList = [
+    {
+        id: 2,
+        name: 'Kivi',
+        amount: 23,
+        unitPrice: 14,
+    },
+];
+const emptyList = [];
+
+function shoppingList(arr) {
+    if (arr.length === 0) {
+        return 'Šiuo metu, jūsų prekių krepšelis yra tuščias.';
+    }
+    console.log('-------------------------------------------------------');
+    console.log('Pavadinimas  | Kiekis  | Vieneto Kaina | Viso mokėti');
+    console.log('-------------------------------------------------------');
+        console.log(`${0 + 1}. ${arr[0].name} | ${arr[0].amount} | ${arr[0].unitPrice * 0.01} Eur      | ${(arr[0].amount * arr[0].unitPrice * 0.01).toFixed(2)} Eur`);  
+        console.log(`${1 + 1}. ${arr[1].name}   | ${arr[1].amount}       | ${arr[1].unitPrice * 0.01} Eur       | ${(arr[1].amount * arr[1].unitPrice * 0.01).toFixed(2)} Eur`);  
+        console.log(`${2 + 1}. ${arr[2].name}  | ${arr[2].amount}       | ${arr[2].unitPrice * 0.01} Eur      | ${(arr[2].amount * arr[2].unitPrice * 0.01).toFixed(2)} Eur`);  
+
+    return  '-------------------------------------------------------';
+
+}
+console.log(shoppingList(emptyList));
+console.log(shoppingList(firstShoppingList));
+
+
+
+
